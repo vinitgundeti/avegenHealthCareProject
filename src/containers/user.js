36b7fc0566675ca33ec539267ef951b044a26be2
Vinit Gundeti <vinit@glideinvest.com>
2023-05-21 12:21:24 +0530

    user screen modified

--- src/containers/user.js
+++ src/containers/user.js
@@ -1,19 +1,54 @@
 import React, {useContext} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import AuthContext from '../config/AuthContext';
 import globalStyles from '../config/globalStyles';
 import UserName from '../components/Common/userName';
+import {Colors} from '../config/constants';
 
+const fileStyle = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.white,
+    flex: 1,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    // marginBottom: 30,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: Colors.black,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  bottomContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default function User() {
   const context = useContext(AuthContext);
   return (
-    <View style={globalStyles.wrapper}>
-      <UserName />
-      <TouchableOpacity
-        style={globalStyles.button}
-        onPress={() => context.logOut()}>
-        <Text style={globalStyles.buttonText}>Logout</Text>
-      </TouchableOpacity>
+    <View
+      style={[fileStyle.container, globalStyles.wrapper, globalStyles.flex1]}>
+      {/* <UserName /> */}
+      <View style={fileStyle.bottomContent}>
+        <Image
+          source={{
+            uri: context.user.img,
+          }}
+          style={fileStyle.avatar}
+        />
+        <Text style={fileStyle.name}>{context.user.name}</Text>
+        <TouchableOpacity
+          style={globalStyles.button}
+          onPress={() => context.logOut()}>
+          <Text style={globalStyles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
